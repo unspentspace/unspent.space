f0427f7f4f67fd070211c5887046de6a6cff6355
dragon-foobar <dragon.foobar@proton.me>
2023-07-11 16:12:50 +1000

    Updates

---
+++ 252f366e-99eeec45e24722fb72f2.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkobsidian_starter=self.webpackChunkobsidian_starter||[]).push([[228],{2775:function(t,a,h){h.d(a,{jhj:function(){return r}});var n=h(1046);function r(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-99eeec45e24722fb72f2.js.map
--- src/components/MDXComponents.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "gatsby";
-
-const MDXComponents = {
-  a: (props) => <Link {...props} />,
-};
-
-export default MDXComponents;
