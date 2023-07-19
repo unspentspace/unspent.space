e73cdaa3f8ed02106e0b2230a85dd8e8cc1a8fa9
dragon-foobar <dragon.foobar@proton.me>
2023-07-19 22:24:41 +1000

    Updates

--- 252f366e-95539817fd8bbb465c84.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkunspent_space_frontend=self.webpackChunkunspent_space_frontend||[]).push([[228],{2775:function(t,n,a){a.d(n,{jhj:function(){return h}});var e=a(1046);function h(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M19 19H5V5h7V3H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"}}]})(t)}}}]);
-//# sourceMappingURL=252f366e-95539817fd8bbb465c84.js.map
